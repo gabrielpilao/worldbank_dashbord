df62d9822a0e0867bf92ce97eb3fc73df4a89e7b
Your-Full-Name <your-email-address>
2020-07-12 14:50:32 -0300

    initial setups

---
+++ worldbankapp/routes.py
@@ -0,0 +1,10 @@
+from worldbankapp import app
+import json
+import plotly
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
